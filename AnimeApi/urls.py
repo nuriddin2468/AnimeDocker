7a2cbe1637e4bb5f58315a1b1a3050955a06ad20
Nuriddin <nuriddin_2468@mail.ru>
2020-01-31 16:54:46 +0500

    Profile App

--- AnimeApi/urls.py
+++ AnimeApi/urls.py
@@ -19,8 +19,12 @@ from rest_framework import routers
 from Posts import views as anim
 from rest_framework.authtoken.views import obtain_auth_token
 from django.views.decorators.csrf import csrf_exempt
+from Profiles import views as prof
+
 
 router = routers.DefaultRouter()
+router.register(r'users', prof.UserViewSet)
+router.register(r'groups', prof.GroupViewSet)
 router.register(r'anime', anim.AnimeViewSet, basename="anime")
 router.register(r'genre', anim.GenreViewSet, basename="genre")
 
