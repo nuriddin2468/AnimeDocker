3c48328a1c75aba837ba7635e7f83a96fd1317b1
Nuriddin <nuriddin_2468@mail.ru>
2020-01-31 11:52:28 +0500

    added ip adresses

--- AnimeApi/settings.py
+++ AnimeApi/settings.py
@@ -25,8 +25,7 @@ SECRET_KEY = '**c7k%1dh(7aux7g+_u#mi-==mpe472w&oyt4cy=!c*jab$k3s'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['damp-wildwood-82425.herokuapp.com', 'localhost', '127.0.0.1']
 
 # Application definition
 
