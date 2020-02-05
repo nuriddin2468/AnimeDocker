7472626e6154a3f060e8a0553752efac446efc67
Nuriddin <nuriddin_2468@mail.ru>
2020-02-05 13:26:05 +0500

    sessions are missing

--- AnimeApi/settings.py
+++ AnimeApi/settings.py
@@ -114,6 +114,8 @@ REST_FRAMEWORK = {
     ),
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+        ''
     ),
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
     'PAGE_SIZE': 10
